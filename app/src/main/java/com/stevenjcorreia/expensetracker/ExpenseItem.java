18573fa2e917c9c04d92209259134cd39b4765b8
Steven Correia <stevenjosephcorreia@gmail.com>
2020-04-12 22:45:02 -0400

    Prototype 2
    Signed-off-by: Steven Correia <stevenjosephcorreia@gmail.com>

--- app/src/main/java/com/stevenjcorreia/expensetracker/ExpenseItem.java
+++ app/src/main/java/com/stevenjcorreia/expensetracker/ExpenseItem.java
@@ -1,14 +1,23 @@
 package com.stevenjcorreia.expensetracker;
 
+import android.content.Context;
+import android.net.Uri;
 import android.util.Log;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.Serializable;
+import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Locale;
 
@@ -31,38 +40,6 @@ public class ExpenseItem implements Serializable {
     ExpenseItem(double price, String category, String date) {
         this.category = category;
         this.date = date;
-        this.price = price;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    int getID() {
-        return ID;
-    }
-
-    String getDate() {
-        return date;
-    }
-
-    double getPrice() {
-        return price;
-    }
-
-    void setID(int ID) {
-        this.ID = ID;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    void setDate(String date) {
-        this.date = date;
-    }
-
-    void setPrice(double price) {
         this.price = price;
     }
 
@@ -104,6 +81,74 @@ public class ExpenseItem implements Serializable {
         }
     };
 
+    static void exportExpenses(ArrayList<ExpenseItem> expenseList, Uri directoryPath, Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.getContentResolver().openOutputStream(directoryPath));
+
+            for (ExpenseItem expense: expenseList) {
+                outputStreamWriter.append(expense.toStringCSV());
+            }
+
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e(TAG, String.format("exportExpenses: Could not write import expenses to \"%s\" directory.", directoryPath), e);
+        }
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    int getID() {
+        return ID;
+    }
+
+    String getDate() {
+        return date;
+    }
+
+    double getPrice() {
+        return price;
+    }
+
+    static ArrayList<ExpenseItem> importExpenses(Uri filePath, Context context) {
+        ArrayList<ExpenseItem> output = new ArrayList<>();
+
+        try (InputStream inputStream = context.getContentResolver().openInputStream(filePath)) {
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String line = "";
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    if (!line.isEmpty()) {
+                        output.add(parse(line));
+                    }
+                }
+            }
+        }
+        catch (IOException e) {
+            Log.e(TAG, String.format("importExpenses: Could not open file \"%s\", returning empty array list.", filePath), e);
+        }
+
+        return output;
+    }
+
+    private static ExpenseItem parse(String line) {
+        // "3,Gas,Feb 8 2020,12.58"
+        int ID = Integer.parseInt(line.split("[,]")[0]);
+
+        String category = line.split("[,]")[1];
+
+        String date = line.split("[,]")[2];
+        date = date.substring(0, date.lastIndexOf(" ")) + "," + date.substring(date.lastIndexOf(" "));
+
+        double price = Double.parseDouble(line.split("[,]")[3]);
+
+        return new ExpenseItem(price, category, date);
+    }
+
     static final Comparator<ExpenseItem> PRICE_ASCENDING = new Comparator<ExpenseItem>() {
         @Override
         public int compare(ExpenseItem o1, ExpenseItem o2) {
@@ -117,4 +162,29 @@ public class ExpenseItem implements Serializable {
             return Double.compare(o2.getPrice(), o1.getPrice());
         }
     };
+
+    void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    void setDate(String date) {
+        this.date = date;
+    }
+
+    void setPrice(double price) {
+        this.price = price;
+    }
+
+    private String toStringCSV() {
+        return String.format(Locale.US,
+                "%d,%s,%s,%s\n",
+                this.ID,
+                this.category,
+                this.date.replace(",", ""),
+                new DecimalFormat("0.00").format(this.price));
+    }
 }
--- app/src/main/java/com/stevenjcorreia/expensetracker/ExpenseItemHolder.java
+++ app/src/main/java/com/stevenjcorreia/expensetracker/ExpenseItemHolder.java
@@ -7,21 +7,28 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ExpenseItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    private boolean imported;
+
     private ExpenseClickListener expenseClickListener;
     TextView expensePrice, expenseCategory, expenseDate;
 
-    ExpenseItemHolder(@NonNull View view) {
+    ExpenseItemHolder(@NonNull View view, boolean imported) {
         super(view);
         expensePrice = view.findViewById(R.id.expensePrice);
         expenseCategory = view.findViewById(R.id.expenseCategory);
         expenseDate = view.findViewById(R.id.expenseDate);
+        this.imported = imported;
 
-        view.setOnClickListener(this);
+        if (!imported) {
+            view.setOnClickListener(this);
+        }
     }
 
     @Override
     public void onClick(View v) {
-        this.expenseClickListener.onExpenseClickListener(v, getLayoutPosition());
+        if (!imported) {
+            this.expenseClickListener.onExpenseClickListener(v, getLayoutPosition());
+        }
     }
 
     void setItemClickListener(ExpenseClickListener expenseClickListener) {
